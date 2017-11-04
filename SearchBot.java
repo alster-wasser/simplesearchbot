600eae0681e82ff7c310899018cc8c859a3c8473
Irina Lindt <lindtdev@gmail.com>
2017-11-04 14:14:00 +0100

    Started working on SearchBot

---
+++ SearchBot.java
@@ -0,0 +1,12 @@
+class SearchBot {
+    private Node startNode;
+
+    public SearchBot(Labyrinth labyrinth){
+        startNode = (Node) labyrinth.getStartField();
+    }
+
+    //@TODO: implement a way to output the labyrinth with the current search state
+    public void printCurrentState(){
+
+    }
+}
