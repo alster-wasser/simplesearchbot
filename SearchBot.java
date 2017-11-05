3b7e8a4f5462d049697929bdbb9ff2819bc58b21
Irina Lindt <lindtdev@gmail.com>
2017-11-05 01:50:50 +0100

    Some more work on frontier. Removed Node class

--- SearchBot.java
+++ SearchBot.java
@@ -2,29 +2,60 @@ import java.util.*;
 
 class SearchBot {
     private Field startField;
-   // private Field currentField;
     private Labyrinth labyrinth;
+    private Frontier frontier;
 
-   // public enum Direction {UP, DOWN, LEFT, RIGHT}
-
+    //Creates a new bot set on the labyrinth.
+    //The bot sits down on the start field
+    //and creates a frontier with just the start field node in it.
     public SearchBot(Labyrinth labyrinth){
         this.labyrinth = labyrinth;
         startField = labyrinth.getStartField();
-       // currentField = startField;
+        frontier = new Frontier(new SearchPath(startField));
     }
 
+    //Since no other class is going to use the SearchPath and Frontier classes,
+    //it seemed appropriate to define them as local classes.
     private class SearchPath {
         private LinkedList<Field> path;
 
-        public Field getHeadNode(){
+        //constructor for private class SearchPath, creates 
+        //a new path with just one node in it
+        SearchPath(Field field){
+            path = new LinkedList<Field>();
+            path.add(field);
+        }
+        Field getHeadNode(){
             return path.getLast();
+        }
+        void addNode(Field field){
+            path.add(field);
+        }
+
+        int getPathLength(){
+            return path.size();
         }
 
     }
-
+    //class Frontier keeps all the search paths
     private class Frontier{
         private ArrayList<SearchPath> pathsInFrontier;
+        
+        Frontier(SearchPath path){
+            pathsInFrontier = new ArrayList<SearchPath>();
+            pathsInFrontier.add(path);
+        }
+        void addPathToFrontier(SearchPath path){
+            pathsInFrontier.add(path);
+        }
+        void removePathFromFrontier(SearchPath path){
+            pathsInFrontier.remove(path);
+        }
 
+        //might not actually be needed but nice to have for testing, see main 
+        int getFrontierSize(){
+            return pathsInFrontier.size();
+        }
     }
 
     //Selecting all possible neighbor nodes, adding only the ones that are passable.
@@ -62,11 +93,13 @@ class SearchBot {
 
     //@TODO: implement a way to output the labyrinth with the current search state
     public void printCurrentState(){
+        System.out.println("test");
     }
 
     public static void main(String[] args){
         Labyrinth labyrinth = new Labyrinth("./blatt3_environment.txt");
         SearchBot bot = new SearchBot(labyrinth);
+        System.out.println(bot.frontier.getFrontierSize());
         bot.printCurrentState();
     }
 }
